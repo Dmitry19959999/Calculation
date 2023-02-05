d24dfa4928edd9a1c094e322d122080efea9ef84
dmitr <dimanmosenko@gmail.com>
2023-02-05 21:21:33 +0300

    add Calculator add AbstractCalculate add ArabCalculator

--- src/src/main/java/org/example/Main.java
+++ src/src/main/java/org/example/Main.java
@@ -2,11 +2,23 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        /* инициализируем класс Reader
+        * */
         Reader reader = new Reader();
+        /* вызываем метод класса Reader read()
+        * */
         String string = reader.read();
 
+        /* инициализируем класс new ArabCalculator() в конструктор прокидываем string
+         * */
         ArabCalculator arabCalculator = new ArabCalculator(string);
+        /* инициализируем класс new Calculate() в конструктор прокидываем string
+         * */
         Calculate calculate = new Calculate();
+        /*
+        Вызываем метод класса Calculate getCalculation()
+        * Для получения результата вызываем методы класса ArabCalculator, которые в сигнатуре метода getCalculation
+        * */
         int result = calculate.getCalculation(arabCalculator.getNumber1(), arabCalculator.getNumber2(), arabCalculator.getDelimeter());
         System.out.println(result);
     }
--- src/src/main/java/org/example/exception/FormatExeption.java
+++ src/src/main/java/org/example/exception/FormatExeption.java
@@ -1,5 +1,8 @@
 package org.example.exception;
 
+/*
+* Создал собственный класс FormatExeption с помощью  "extends Exception"
+* */
 public class FormatExeption extends Exception {
     public FormatExeption(String message) {
         super(message);
